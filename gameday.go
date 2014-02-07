c24474a6a7f10cbbfed4c4f472d739275846f1eb
Kyle Conroy <kyle@kyleconroy.com>
2014-02-06 18:59:09 -0800

    Add GameCenter parsing

--- gameday.go
+++ gameday.go
@@ -2,18 +2,73 @@ package gameday
 
 import (
 	"encoding/xml"
+	"io"
 	"io/ioutil"
-    "io"
+)
+
+const (
+	Angels    = "108"
+	Astros    = "117"
+	Athletics = "133"
+	BlueJays  = "141"
+	Braves    = "144"
+	Brewers   = "158"
+	Cardinals = "138"
+	Cubs      = "112"
+	Dbacks    = "109"
+	Dodgers   = "119"
+	Giants    = "137"
+	Indians   = "114"
+	Mariners  = "136"
+	Marlins   = "146"
+	Mets      = "121"
+	Nationals = "120"
+	Orioles   = "110"
+	Padres    = "135"
+	Phillies  = "143"
+	Pirates   = "134"
+	Rangers   = "140"
+	Rays      = "139"
+	RedSox    = "111"
+	Reds      = "113"
+	Rockies   = "115"
+	Royals    = "118"
+	Tigers    = "116"
+	Twins     = "142"
+	WhiteSox  = "145"
+	Yankees   = "147"
 )
 
 type Game struct {
-	Id string
+	Id           string `xml:"id,attr"`
+	Venue        string `xml:"venue,attr"`
+	Key          string `xml:"game_pk,attr"`
+	Timezone     string `xml:"time_zone,attr"`
+	AwayTeamCode string `xml:"away_code,attr"`
+	AwayTeamId   string `xml:"away_team_id,attr"`
+	AwayTeamName string `xml:"away_team_name,attr"`
+	HomeTeamCode string `xml:"home_code,attr"`
+	HomeTeamId   string `xml:"home_team_id,attr"`
+	HomeTeamName string `xml:"home_team_name,attr"`
 }
 
 type Schedule struct {
-	Games []Game
-    Date string `xml:"date,attr"`
-    DisplayTimeZone string `xml:"display_time_zone,attr"`
+	Games           []Game `xml:"game"`
+	Date            string `xml:"date,attr"`
+	DisplayTimeZone string `xml:"display_time_zone,attr"`
+}
+
+type Broadcast struct {
+        Radio string `xml:"radio"`
+        TV string `xml:"tv"`
+}
+
+type Gamecenter struct {
+        Id string `xml:"id,attr"`
+        VenueShort string `xml:"venueShort"`
+        VenueLong string `xml:"venueLong"`
+        HomeBroadcast Broadcast `xml:"broadcast>home"`
+        AwayBroadcast Broadcast `xml:"broadcast>away"`
 }
 
 func LoadSchedule(reader io.Reader) (Schedule, error) {
@@ -32,4 +87,58 @@ func LoadSchedule(reader io.Reader) (Schedule, error) {
 	}
 
 	return schedule, nil
+}
+
+func LoadGamecenter(reader io.Reader) (Gamecenter, error) {
+	var center Gamecenter
+
+	blob, err := ioutil.ReadAll(reader)
+
+	if err != nil {
+		return center, err
+	}
+
+	err = xml.Unmarshal(blob, &center)
+
+	if err != nil {
+		return center, err
+	}
+
+	return center, nil
+}
+
+func TeamSubreddit(id string) string {
+	subs := map[string]string{
+		Angels:    "/r/AngelsBaseball",
+		Astros:    "/r/Astros",
+		Athletics: "/r/oaklandathletics",
+		BlueJays:  "/r/TorontoBlueJays",
+		Braves:    "/r/Braves",
+		Brewers:   "/r/Brewers",
+		Cardinals: "/r/Cardinals",
+		Cubs:      "/r/Cubs",
+		Dbacks:    "/r/azdiamondbacks",
+		Dodgers:   "/r/Dodgers",
+		Giants:    "/r/SFGiants",
+		Indians:   "/r/WahoosTipi",
+		Mariners:  "/r/Mariners",
+		Marlins:   "/r/letsgofish",
+		Mets:      "/r/NewYorkMets",
+		Nationals: "/r/Nationals",
+		Orioles:   "/r/Orioles",
+		Padres:    "/r/Padres",
+		Phillies:  "/r/Phillies",
+		Pirates:   "/r/Buccos",
+		Rangers:   "/r/TexasRangers",
+		Rays:      "/r/TampaBayRays",
+		RedSox:    "/r/RedSox",
+		Reds:      "/r/Reds",
+		Rockies:   "/r/ColoradoRockies",
+		Royals:    "/r/KCRoyals",
+		Tigers:    "/r/MotorCityKitties",
+		Twins:     "/r/MinnesotaTwins",
+		WhiteSox:  "/r/WhiteSox",
+		Yankees:   "/r/NYYankees",
+	}
+	return subs[id]
 }
--- gameday_test.go
+++ gameday_test.go
@@ -1,28 +1,64 @@
 package gameday
 
 import (
+	"os"
+	"reflect"
 	"testing"
-    "os"
 )
 
+func check(t *testing.T, a interface{}, b interface{}) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("'%+v' != '%+v'", a, b)
+	}
+}
+
 func TestSchedule(t *testing.T) {
-    handle, err := os.Open("fixtures/2013_07_13_epg.xml")
+	handle, err := os.Open("fixtures/2013_07_13_epg.xml")
 
-    if err != nil {
-            t.Fatal(err)
-    }
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    s, err := LoadSchedule(handle)
+	s, err := LoadSchedule(handle)
 
-    if err != nil {
-            t.Fatal(err)
-    }
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if s.Date != "20130714" {
-            t.Errorf("Expected the date to be '20130714' not '%s'", s.Date)
-    }
+	check(t, s.Date, "20130714")
+	check(t, s.DisplayTimeZone, "ET")
+	check(t, len(s.Games), 15)
 
-    if s.DisplayTimeZone != "ET" {
-            t.Errorf("Expected the date to be 'ET' not '%s'", s.DisplayTimeZone)
-    }
+	game := s.Games[0]
+
+	check(t, game.Id, "2013/07/14/kcamlb-clemlb-1")
+	check(t, game.Venue, "Progressive Field")
+	check(t, game.Key, "348159")
+	check(t, game.Timezone, "ET")
+	check(t, game.AwayTeamId, Royals)
+	check(t, game.AwayTeamCode, "kca")
+	check(t, game.HomeTeamId, Indians)
+	check(t, game.HomeTeamCode, "cle")
+}
+
+func TestGamecenter(t *testing.T) {
+	handle, err := os.Open("fixtures/2013_07_14_minmlb_nyamlb_1_gamecenter.xml")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gc, err := LoadGamecenter(handle)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+    check(t, gc.Id, "2013_07_14_minmlb_nyamlb_1")
+	check(t, gc.VenueShort, "Yankee Stadium")
+	check(t, gc.VenueShort, "Yankee Stadium")
+	check(t, gc.HomeBroadcast.Radio, "WCBS 880, WADO 1280")
+	check(t, gc.HomeBroadcast.TV, "YES")
+	check(t, gc.AwayBroadcast.Radio, "96.3 K-TWIN, TIBN, BOB FM")
+	check(t, gc.AwayBroadcast.TV, "FS-N")
 }
