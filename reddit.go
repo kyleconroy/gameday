5e1039852f5b1ecee0637039a7de3ce17fca722a
Matthew Gumport <tropicalmug@gmail.com>
2014-02-12 18:54:10 -0800

    Better basic framework.

--- reddit.go
+++ reddit.go
@@ -2,34 +2,43 @@ package reddit
 
 import (
   "net/http"
+  "net/http/cookiejar"
   "encoding/json"
   "io"
+  "log"
 )
 
 type Client struct {
   Username, Password, Hash  string
-  hclient                    http.Client
+  hclient                   http.Client
+  jar                   http.CookieJar
 }
 
 func New(u string, p string) *Client {
-  c := Char{Username: u, Password: p}
-  payload = {'user': u, 'passwd': p}
-  c.login(payload)
+  c := interface{Username: u, Password: p}
+  c.login()
   return &c
 }
 
 func (c *Client) login () {
-  c.hclient = &http.Client{}
+  c.jar, err := cookiejar.New(nil)
+  if err := nil {
+    log.Fatal(err)
+  }
+  c.hclient = http.Client{Jar: c.jar}
 
-  c.Post('https://ssl.reddit.com/api/login')  
 }
 
-func (c *Client) Post (url string) {
+func (c *Client) header_update() {
+
+}
+
+func (c *Client) Post (url, payload string) (*Response, error) {
   return c.generate_request("POST", url, nil)
 }
 
 
-func (c *Client) Get (url string) {
+func (c *Client) Get (url string) (*Response, error) {
   return c.generate_request("GET", url, nil)
 }
 
@@ -39,5 +48,12 @@ func (c *Client) generate_request (t string, url string, body io.Reader) {
     return req
   } else {
     return err
+  }
+  make_request(c.Do, req)
+}
+
+func (c *Client) make_request(fp func(req *Request) (resp *Response, err error), r *Request) (*Response, error) {
+  for i := 0, i < 5, i++ {
+
   }
 }
